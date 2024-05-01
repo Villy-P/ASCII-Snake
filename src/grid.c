6c517ed0c1c307b6b6c8b70cd2081f4305b168fe
Villy-P <valeriuspetrini@gmail.com>
2024-04-30 19:52:29 -0500

    Print full grid

--- src/grid.c
+++ src/grid.c
@@ -8,6 +8,16 @@ int gridHeight = 15;
 void displayGrid() {
     printf("+");
     for (int i = 0; i < gridWidth; i++)
-        printf("-");
-    printf("+\n");
+        printf(" -");
+    printf(" +\n");
+    for (int i = 0; i < gridHeight; i++) {
+        printf("|");
+        for (int i = 0; i < gridWidth; i++)
+            printf("  ");
+        printf(" |\n");
+    }
+    printf("+");
+    for (int i = 0; i < gridWidth; i++)
+        printf(" -");
+    printf(" +\n");
 }
